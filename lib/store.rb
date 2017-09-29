fb8abc38e4b82764a5a9cd3e363a05f528045cfa
mo <meganannetteolson@yahoo.com>
2017-09-29 10:45:39 -0700

    display price integer as currency

--- lib/store.rb
+++ lib/store.rb
@@ -2,7 +2,7 @@ class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
   validates(:name, :presence => true)
   validates(:name, {:presence => true, :length => { :maximum => 100 }})
-  validates :name, uniqueness: true
+  validates(:name, uniqueness: true)
   before_save(:capitalize_first_letter)
 
 
