6197d37658de078c86b9d784a62aab4dc99bf1ea
mo <meganannetteolson@yahoo.com>
2017-09-29 10:20:24 -0700

    pass validations and callbacks previously done for Stores in Brands

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,16 +1,16 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
-  # validates(:title, :presence => true)
-  # validates(:instructions, :presence => true)
-  # validates(:title, {:presence => true, :length => { :maximum => 100 }})
-  # before_save(:capitalize_first_letter)
-  #
-  #
-  # private
-  #
-  # def capitalize_first_letter
-  #   title = self.title.split(" ")
-  #   self.title = title.map{|word| word.capitalize}.join(" ")
-  # end
+  validates(:name, :presence => true)
+  validates(:price, :presence => true)
+  validates(:name, {:presence => true, :length => { :maximum => 100 }})
+  before_save(:capitalize_first_letter)
+
+
+  private
+
+  def capitalize_first_letter
+    name = self.name.split(" ")
+    self.name = name.map{|word| word.capitalize}.join(" ")
+  end
 
 end
---
+++ spec/brand_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe(Brand) do
+  it("validates presence of name") do
+    brand = Brand.new({:name => ""})
+    expect(brand.save()).to(eq(false))
+  end
+
+  it("ensures the length of title is at most 100 characters") do
+    brand = Brand.new({:name => "a".*(101)})
+    expect(brand.save()).to(eq(false))
+  end
+
+  it("converts the first letter of the brand name to uppercase") do
+    brand = Brand.new({:name => "under armour", :price => 80})
+    brand.save
+    expect(brand.name()).to(eq("Under Armour"))
+  end
+end
